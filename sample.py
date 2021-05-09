1654c1086e0bff5ff37b6f3edd0544b348069203
takamotobiz <keiji.takamoto@takamoto.biz>
2021-05-10 08:29:48 +0900

    main

--- sample.py
+++ sample.py
@@ -1,22 +1,23 @@
 import numpy as np
 from osgeo import gdal, gdalconst, gdal_array
 
-src = gdal.Open('/Users/takamotokeiji/data/geotiff/gm_el_v1.tif', gdalconst.GA_ReadOnly) # tifの読み込み (read only)
+src = gdal.Open('./data/gm_el_v1.tif', gdalconst.GA_ReadOnly) # tifの読み込み (read only)
 type(src) # "osgeo.gdal.Dataset"
 
-src.RasterXSize # 水平方向ピクセル数
-src.RasterYSize # 鉛直方向ピクセル数
-src.RasterCount # バンド数
+print("水平Pix：" ,src.RasterXSize) # 水平方向ピクセル数
+print("垂直Pix：" ,src.RasterYSize) # 鉛直方向ピクセル数
+print("バンド数：" ,src.RasterCount) # バンド数
 
 b1 = src.GetRasterBand(1).ReadAsArray() # 第１バンド numpy array
 
 dtid = src.GetRasterBand(1).DataType # 型番号 (ex: 6 -> numpy.float32)
-gdal_array.GDALTypeCodeToNumericTypeCode(dtid) # 型番号 -> 型名 変換
+print("型番号：" ,dtid)
+print("型名　：" ,gdal_array.GDALTypeCodeToNumericTypeCode(dtid)) # 型番号 -> 型名 変換
 
 # others...
 
-src.GetRasterBand(1).GetNoDataValue()
-src.GetRasterBand(1).GetMinimum()
-src.GetRasterBand(1).GetMaximum()
-src.GetRasterBand(1).GetScale()
-src.GetRasterBand(1).GetUnitType()
+print("GetNoDataValue：" ,src.GetRasterBand(1).GetNoDataValue())
+print("GetMinimum：" ,src.GetRasterBand(1).GetMinimum())
+print("GetMaximum：" ,src.GetRasterBand(1).GetMaximum())
+print("GetScale：" ,src.GetRasterBand(1).GetScale())
+print("GetUnitType：" ,src.GetRasterBand(1).GetUnitType())
